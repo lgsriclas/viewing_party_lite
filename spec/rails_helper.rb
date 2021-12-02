2bcaecd500967cd59a27fbea6a082f1d6341f92e
Jacob Yarborough <jayarborough95@gmail.com>
2021-12-02 14:38:33 -0700

    Configure VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -72,3 +73,20 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<movie_api_key>') { ENV['movie_api_key'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+end
+
+def data_1 
+  # put data from api call here
+end 
+def data_2
+  # put data from api call here
+end 
+def data_3
+  # put data from api call here
+end 
