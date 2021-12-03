b61b5b5b76d26b8dc5830471c7c09068447f8330
Lesley Sanders <lesleyasanders@gmail.com>
2021-12-03 13:22:50 -0500

    Update new viewing party spec

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -8,15 +8,17 @@ RSpec.describe 'create viewing party' do
 
         visit "/users/#{user.id}/movies/2/party/new"
 
+        fill_in :movie_title, with: "Ariel"
+        fill_in :duration, with: 73
         fill_in :start_date, with: '12/03/2021'
         fill_in :start_time, with: '08:30 PM'
+        choose(nil)
         click_button 'Create Party'
 
         expect(page.status_code).to eq 200
         expect(page).to have_content("Ariel")
-        expect(page).to have_content("73")
-        expect(page).to have_content("2021-03-12")
-        expect(page).to have_content("22:30 PM")
+        expect(page).to have_content("December 3, 2021")
+        expect(page).to have_content("8:30 PM")
       end
     end
   end
